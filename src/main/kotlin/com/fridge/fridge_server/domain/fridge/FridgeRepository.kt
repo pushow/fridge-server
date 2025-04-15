d672388eef64b84d0abc485f89ca0ac1fd0d6cd8
pushow <qkrehdals55@gmail.com>
2025-04-15 19:44:00 +0900

    feat: 가족정보로 냉장고 불러오기

--- src/main/kotlin/com/fridge/fridge_server/domain/fridge/FridgeRepository.kt
+++ src/main/kotlin/com/fridge/fridge_server/domain/fridge/FridgeRepository.kt
@@ -1,2 +1,8 @@
 package com.fridge.fridge_server.domain.fridge
 
+import com.fridge.fridge_server.domain.family.FamilyGroup
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface FridgeRepository : JpaRepository<Fridge, Long> {
+    fun findAllByFamilyGroup(familyGroup: FamilyGroup): List<Fridge>
+}
--- src/main/kotlin/com/fridge/fridge_server/domain/fridge/FridgeService.kt
+++ src/main/kotlin/com/fridge/fridge_server/domain/fridge/FridgeService.kt
@@ -1,2 +1,11 @@
 package com.fridge.fridge_server.domain.fridge
 
+import com.fridge.fridge_server.domain.family.FamilyGroup
+import org.springframework.stereotype.Service
+
+@Service
+class FridgeService(private val fridgeRepository: FridgeRepository) {
+    fun getFridgesByFamilyGroup(familyGroup: FamilyGroup): List<Fridge> {
+        return fridgeRepository.findAllByFamilyGroup(familyGroup)
+    }
+}
