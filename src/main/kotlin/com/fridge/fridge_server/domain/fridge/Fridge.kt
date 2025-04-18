45581958163d5c0d0c0be2fc93b9f3438b9a8755
pushow <qkrehdals55@gmail.com>
2025-04-19 03:20:13 +0900

    feat: 냉장고가 삭제될 시 내부 음식도삭제

--- src/main/kotlin/com/fridge/fridge_server/domain/fridge/Fridge.kt
+++ src/main/kotlin/com/fridge/fridge_server/domain/fridge/Fridge.kt
@@ -1,6 +1,7 @@
 package com.fridge.fridge_server.domain.fridge
 
 import com.fridge.fridge_server.domain.family.FamilyGroup
+import com.fridge.fridge_server.domain.food.Food
 import jakarta.persistence.*
 
 @Entity
@@ -13,5 +14,8 @@ class Fridge(
 
     @ManyToOne
     @JoinColumn(name = "family_group_id")
-    val familyGroup: FamilyGroup
+    val familyGroup: FamilyGroup,
+
+    @OneToMany(mappedBy = "fridge", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val foods: List<Food> = mutableListOf()
 )
